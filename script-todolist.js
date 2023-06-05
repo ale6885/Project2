efdc2100b9384b9c0e9c17d160a5bbf8bfade746
ale6885 <123508556+ale6885@users.noreply.github.com>
2023-06-05 23:59:59 +0200

    Add files via upload

---
+++ script-todolist.js
@@ -0,0 +1,58 @@
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+const todoButton = document.getElementById('todo-button');
+
+todoForm.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  const todoText = todoInput.value.trim(); 
+
+  if (todoText !== '') {
+    const li = document.createElement('li');
+
+    const span = document.createElement('span');
+    span.textContent = todoText; 
+
+    const buttonDelete = document.createElement('button');
+    buttonDelete.textContent = 'Delete'; 
+
+    buttonDelete.addEventListener('click', function() {
+      li.remove(); 
+    });
+
+    li.appendChild(span);
+    li.appendChild(buttonDelete);
+
+    todoList.appendChild(li);
+
+    todoInput.value = '';
+  }
+});
+
+todoButton.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const todoText = todoInput.value.trim();
+
+  if (todoText !== '') {
+    const li = document.createElement('li');
+
+    const span = document.createElement('span');
+    span.textContent = todoText;
+
+    const buttonDelete = document.createElement('button');
+    buttonDelete.textContent = 'Delete';
+
+    buttonDelete.addEventListener('click', function() {
+      li.remove();
+    });
+
+    li.appendChild(span);
+    li.appendChild(buttonDelete);
+
+    todoList.appendChild(li);
+
+    todoInput.value = '';
+  }
+});
