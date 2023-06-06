208e11242d86b73deec9fe9a22467bfb5a386f11
ale6885 <123508556+ale6885@users.noreply.github.com>
2023-06-06 22:16:29 +0200

    last changes

--- script-game.js
+++ script-game.js
@@ -1,52 +1,51 @@
-// Get the choice buttons, result message element, and reset button
-const choiceButtons = document.querySelectorAll('.choice');
-const resultMessage = document.querySelector('.result-message');
-const resetButton = document.querySelector('.reset-button');
+  const choiceButtons = document.querySelectorAll('.choice');
+    const resultMessage = document.querySelector('.result-message');
+    const resetButton = document.querySelector('.reset-button');
 
-// Add event listeners for choice buttons
-choiceButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const playerChoice = button.dataset.choice;
-    const computerChoice = getRandomChoice();
-    const result = getGameResult(playerChoice, computerChoice);
 
-    displayResult(playerChoice, computerChoice, result);
-  });
-});
+    choiceButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const playerChoice = button.dataset.choice;
+        const computerChoice = getRandomChoice();
+        const result = getGameResult(playerChoice, computerChoice);
 
-// Add event listener for reset button
-resetButton.addEventListener('click', () => {
-  resetGame();
-});
+        displayResult(playerChoice, computerChoice, result);
+      });
+    });
 
-// Function to get a random computer choice
-function getRandomChoice() {
-  const choices = ['rock', 'paper', 'scissors'];
-  const randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex];
-}
 
-// Function to determine the game result
-function getGameResult(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    return 'draw';
-  } else if (
-    (playerChoice === 'rock' && computerChoice === 'scissors') ||
-    (playerChoice === 'paper' && computerChoice === 'rock') ||
-    (playerChoice === 'scissors' && computerChoice === 'paper')
-  ) {
-    return 'win';
-  } else {
-    return 'lose';
+    resetButton.addEventListener('click', () => {
+      resetGame();
+    });
+
+
+    function getRandomChoice() {
+      const choices = ['rock', 'paper', 'scissors'];
+      const randomIndex = Math.floor(Math.random() * choices.length);
+      return choices[randomIndex];
+    }
+
+
+    function getGameResult(playerChoice, computerChoice) {
+      if (playerChoice === computerChoice) {
+        return 'draw';
+      } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+      ) {
+        return 'win';
+      } else {
+        return 'lose';
+      }
+    }
+
+
+  function displayResult(playerChoice, computerChoice, result) {
+    resultMessage.textContent = `Player: ${playerChoice} | Computer: ${computerChoice} | Result: ${result}`;
   }
-}
 
-// Function to display the game result
-function displayResult(playerChoice, computerChoice, result) {
-  resultMessage.textContent = `Player: ${playerChoice} | Computer: ${computerChoice} | Result: ${result}`;
-}
 
-// Function to reset the game
-function resetGame() {
-  resultMessage.textContent = '';
-}
+  function resetGame() {
+    resultMessage.textContent = '';
+  }
--- script-todolist.js
+++ script-todolist.js
@@ -1,58 +1,58 @@
-const todoForm = document.getElementById('todo-form');
-const todoInput = document.getElementById('todo-input');
-const todoList = document.getElementById('todo-list');
-const todoButton = document.getElementById('todo-button');
+  const todoForm = document.getElementById('todo-form');
+  const todoInput = document.getElementById('todo-input');
+  const todoList = document.getElementById('todo-list');
+  const todoButton = document.getElementById('todo-button');
 
-todoForm.addEventListener('submit', function(event) {
-  event.preventDefault(); 
+  todoForm.addEventListener('submit', function(event) {
+    event.preventDefault(); 
 
-  const todoText = todoInput.value.trim(); 
+    const todoText = todoInput.value.trim(); 
 
-  if (todoText !== '') {
-    const li = document.createElement('li');
+    if (todoText !== '') {
+      const li = document.createElement('li');
 
-    const span = document.createElement('span');
-    span.textContent = todoText; 
+      const span = document.createElement('span');
+      span.textContent = todoText; 
 
-    const buttonDelete = document.createElement('button');
-    buttonDelete.textContent = 'Delete'; 
+      const buttonDelete = document.createElement('button');
+      buttonDelete.textContent = 'Delete'; 
 
-    buttonDelete.addEventListener('click', function() {
-      li.remove(); 
-    });
+      buttonDelete.addEventListener('click', function() {
+        li.remove(); 
+      });
 
-    li.appendChild(span);
-    li.appendChild(buttonDelete);
+      li.appendChild(span);
+      li.appendChild(buttonDelete);
 
-    todoList.appendChild(li);
+      todoList.appendChild(li);
 
-    todoInput.value = '';
-  }
-});
+      todoInput.value = '';
+    }
+  });
 
-todoButton.addEventListener('click', function(event) {
-  event.preventDefault();
+  todoButton.addEventListener('click', function(event) {
+    event.preventDefault();
 
-  const todoText = todoInput.value.trim();
+    const todoText = todoInput.value.trim();
 
-  if (todoText !== '') {
-    const li = document.createElement('li');
+    if (todoText !== '') {
+      const li = document.createElement('li');
 
-    const span = document.createElement('span');
-    span.textContent = todoText;
+      const span = document.createElement('span');
+      span.textContent = todoText;
 
-    const buttonDelete = document.createElement('button');
-    buttonDelete.textContent = 'Delete';
+      const buttonDelete = document.createElement('button');
+      buttonDelete.textContent = 'Delete';
 
-    buttonDelete.addEventListener('click', function() {
-      li.remove();
-    });
+      buttonDelete.addEventListener('click', function() {
+        li.remove();
+      });
 
-    li.appendChild(span);
-    li.appendChild(buttonDelete);
+      li.appendChild(span);
+      li.appendChild(buttonDelete);
 
-    todoList.appendChild(li);
+      todoList.appendChild(li);
 
-    todoInput.value = '';
-  }
-});
+      todoInput.value = '';
+    }
+  });
